bd63f4a7501c61d306c55a89ea586cf3d99b6d5d
Himanshu Maurya <mauryahimanshu1999@gmail.com>
2023-09-30 19:17:05 +0530

    json web token installed

--- models/userModel.js
+++ models/userModel.js
@@ -30,7 +30,7 @@ var userSchema = new mongoose.Schema({
 });
 
 userSchema.pre('save', async function (next) {
-  const salt = await bcreypt.genSaltSync(10);
+  const salt = await bcrypt.genSaltSync(10);
   this.password = await bcrypt.hash(this.password, salt);
 });
 
